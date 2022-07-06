ec4b8313b20b2fe7e5276a942a61f07fc09b393b
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-07-06 17:48:00 +0300

    Reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -12,7 +12,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.annotate(Count('posts'))],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -20,7 +20,7 @@ def serialize_post(post):
 def serialize_tag(tag):
     return {
         'title': tag.title,
-        'posts_with_tag': len(Post.objects.filter(tags=tag)),
+        'posts_with_tag': tag.posts__count,
     }
 
 
@@ -32,13 +32,14 @@ def index(request):
                                      .fetch_with_comments_count()
     
     fresh_posts = Post.objects.annotate(comments_count=Count('comments', distinct=True))\
-                                        .order_by('published_at')\
+                                        .order_by('-published_at')\
                                         .prefetch_related('author')\
                                         .prefetch_related('tags')
                                         
-    most_fresh_posts = list(fresh_posts)[-5:]
+    most_fresh_posts = list(fresh_posts)[:5]
 
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = Tag.objects.annotate(post_count=Count('posts'))\
+                                    .popular()[:5]
 
     context = {
         'most_popular_posts': [
